eaa4020f231356d254141db265748291e0e4120a
Robin Oester <robin.oester@gmail.com>
2023-04-04 21:37:05 +0200

    Edit test.py - Added main() method - Added test() function to demonstrate useful stuff

--- test.py
+++ test.py
@@ -1,23 +1,46 @@
-
 #!/usr/bin/env python3
 
 import subprocess
 import re
 
-benchmark = "parsec-blackscholes"
-a = r"real\s*([0-9]*)m(\d*.\d*)s"
-output = subprocess.run(f"""kubectl logs $(kubectl get pods --selector=job-name={benchmark} --output=jsonpath='{{.items[*].metadata.name}}\')""", shell=True, capture_output=True)
+
+def test():
+    a = r"real\s*(\d*)m(\d*.\d*)s"
+    out = subprocess.run("ls", shell=True, capture_output=True).stdout.decode("utf-8")
+    with open("output.txt", "w") as file:
+        file.write(out)
+
+    measurement = re.findall(a, """Num of Runs: 100
+        Size of data: 2621440
+
+        real    2m1.117s
+        user    0m1.062s
+        sys     0m0.008s""")[0]
+
+    seconds = int(measurement[0]) * 60. + float(measurement[1])
+
+    print(seconds)
 
 
-print(re.findall(a, """Num of Runs: 100
-Size of data: 2621440
+def main():
+    benchmark = "parsec-blackscholes"
+    a = r"real\s*([0-9]*)m(\d*.\d*)s"
+    output = subprocess.run(f"""kubectl logs $(kubectl get pods --selector=job-name={benchmark} --output=jsonpath='{{.items[*].metadata.name}}\')""", shell=True, capture_output=True)
 
-real    0m1.117s
-user    0m1.062s
-sys     0m0.008s""")[0])
+    print(re.findall(a, """Num of Runs: 100
+    Size of data: 2621440
 
-print("Deleting job...")
+    real    0m1.117s
+    user    0m1.062s
+    sys     0m0.008s""")[0])
 
-print(subprocess.run(f"kubectl delete jobs/{benchmark}", shell=True))
+    print("Deleting job...")
 
-print("Done")
+    print(subprocess.run(f"kubectl delete jobs/{benchmark}", shell=True))
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    # main()
+    test()
